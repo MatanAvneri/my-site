8f71554891634a740b7fb2503047910f82056ec9
matanavneri <matan.av@gmail.com>
2019-10-21 00:18:47 +0200

    add positions

---
+++ src/Timeline/config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    iconSrc: 'https://media.licdn.com/dms/image/C4D0BAQEmGve5u2GOtQ/company-logo_400_400/0?e=1579737600&v=beta&t=7-Sth7aR5jVkji_ocr6PaAjhV3jlg9nd0lRnQMhLafA',
+    title: 'R&D Team Leader',
+    company: 'ZOOZ',
+    start: 'Feb 2018',
+    end: 'Present'
+  },
+  {
+    iconSrc: 'https://media.licdn.com/dms/image/C510BAQH45nKtEzFopw/company-logo_400_400/0?e=1579737600&v=beta&t=RQXF4j_6Wl4MYb5OzwCFwN8npdqihZ5GX4Zp1A3Ka5Q',
+    title: 'Web Application Team Leader',
+    company: 'eXelate, A Nielsen Company',
+    start: 'Jul 2015',
+    end: 'Feb 2018',
+    description: `
+    Leading the DMP Core R&D team for Nielsen Marketing Cloud.
+    On a Dockerized environment and AWS technology stack with a micro services architecture
+    `
+  },
+  {
+    iconSrc: 'https://media.licdn.com/dms/image/C510BAQH45nKtEzFopw/company-logo_400_400/0?e=1579737600&v=beta&t=RQXF4j_6Wl4MYb5OzwCFwN8npdqihZ5GX4Zp1A3Ka5Q',
+    title: 'Front End Developer',
+    company: 'eXelate, A Nielsen Company',
+    start: 'Oct 2013',
+    end: 'Jul 2015'
+  },
+  {
+    iconSrc: 'https://media.licdn.com/dms/image/C560BAQFc_VNUvqpWcw/company-logo_400_400/0?e=1579737600&v=beta&t=gJ2ttuQCjVMOI1EwIPK6rBnVoWCqe1eCjpA4Y7z1T1E',
+    title: 'Software Architect',
+    company: 'IDF \ Intelligence Corps \ 9900',
+    start: 'Apr 2011',
+    end: 'Sep 2013'
+  },
+  {
+    iconSrc: 'https://media.licdn.com/dms/image/C560BAQFc_VNUvqpWcw/company-logo_400_400/0?e=1579737600&v=beta&t=gJ2ttuQCjVMOI1EwIPK6rBnVoWCqe1eCjpA4Y7z1T1E',
+    title: 'Shob programmer',
+    company: 'IDF \ Intelligence Corps \ 9900',
+    start: 'Jun 2009',
+    end: 'Apr 2011'
+  }
+]
