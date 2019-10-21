e0cd763da0d80e330f92eed8acab9e6713cb95ef
matanavneri <matan.av@gmail.com>
2019-10-21 16:26:40 +0200

    small cosmetic changes

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ const App = () => {
           Experienced team leader with a proven track record of building high quality, large scale and complex front end systems from scratch.
             <br />
           Skilled in FrontEnd, NodeJS, Java and Management.
-          </p>
-        <img className={style.App__summary__profile} src='https://secure.gravatar.com/avatar/ae59dab97c135533ccd77689d9e8d546?size=200' alt='my profile' />
+          <img className={style.App__summary__profile} src='https://secure.gravatar.com/avatar/ae59dab97c135533ccd77689d9e8d546?size=200' alt='my profile' />
+        </p>
       </header>
       <Timeline />
       <SocialBar />
