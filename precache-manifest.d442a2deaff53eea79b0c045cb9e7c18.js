dcfb59c3fda7da806f5644b52ccf31fa5e126b46
matanavneri <matan.av@gmail.com>
2019-10-21 13:26:44 +0200

    Updates

---
+++ precache-manifest.d442a2deaff53eea79b0c045cb9e7c18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaae690f2c335b5763d861f206f06678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85aa259a7a91f35950c9",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "afdacdc383d16b6c6295",
+    "url": "/static/css/main.b0321380.chunk.css"
+  },
+  {
+    "revision": "85aa259a7a91f35950c9",
+    "url": "/static/js/2.b26dd520.chunk.js"
+  },
+  {
+    "revision": "afdacdc383d16b6c6295",
+    "url": "/static/js/main.c3839223.chunk.js"
+  },
+  {
+    "revision": "56ceb246176a92e99b10",
+    "url": "/static/js/runtime-main.321ccd09.js"
+  },
+  {
+    "revision": "10f616c9d5d1c950c502a1b1174a41d1",
+    "url": "/static/media/logo.10f616c9.png"
+  }
+]);
