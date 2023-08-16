0f24fbb5839526a027d92a1f71a11bb550c2e808
Matan Avneri <matan.av@gmail.com>
2023-08-16 20:49:38 +0300

    feat: migrate to Vite (#75)

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      overlay: { initialIsOpen: false },
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
